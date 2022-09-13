3683b43a8aa21d0405c267a4931dbc548700fcf3
SThevenot <sktmh007@gmail.com>
2022-09-13 08:38:10 -0500

    created .env

---
+++ Develop/models/index.js
@@ -0,0 +1,31 @@
+/** @format */
+
+// import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+
+});
+// Categories have many Products
+Category.hasMany(Product, {
+
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  
+});
+// Tags belongToMany Products (through ProductTag)
+ProductTag.belongsToMany(Product, {
+  
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
